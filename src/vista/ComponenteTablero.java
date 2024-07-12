e3370dc0a5a7dc54872b57aef3d591daa7b0120b
frank <ochoa.franco132@gmail.com>
2024-07-12 17:40:40 -0300

    starting board interface from scratch

--- src/vista/ComponenteTablero.java
+++ src/vista/ComponenteTablero.java
@@ -4,7 +4,24 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ComponenteTablero extends JPanel {
-    
+    private int rows = 10;
+    private int cols = 10;
+    public ComponenteTablero() {
+        setLayout(new GridLayout(rows, cols));
+        setSize(750, 300);
+        setOpaque(false); // muestra el bg
+    }
+
+    public void agregarFicha(VistaFicha ficha) {
+        add(ficha.setSize(100, 100));
+    }
+
+    public void limpiarFicha() {
+    }
+
+    public void setCantFichasTablero(int cantFichasTablero) {
+
+    }
 }
 
 
