673513fdc3a03cc8333eb0ec19bc8c0277b1f009
frank <ochoa.franco132@gmail.com>
2024-07-15 16:18:56 -0300

    adding vertical tiles

--- src/vista/ComponenteTablero.java
+++ src/vista/ComponenteTablero.java
@@ -4,10 +4,11 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ComponenteTablero extends JPanel {
-    private int cantFichasTablero = 0;
+    private int cantFichasTableroHorizontales = 0;
+    private final int CANTFICHASHORIZONTALESMAX = 5;
     private final JPanel segundoPanel;
-    private int lastAddedX = 0; // Initialize with the starting x-position
-    private int lastAddedY = 0; // Initialize with the starting y-position
+    private int ultimoXAgregado = 0; // Initialize with the starting x-position
+    private int ultimoYAgregado = 0; // Initialize with the starting y-position
     private final int ROTAR = 5;
     public ComponenteTablero() {
         setLayout(new BorderLayout());
@@ -20,59 +21,55 @@ public class ComponenteTablero extends JPanel {
     }
 
     public int getCantFichasTablero() {
-        return cantFichasTablero;
+        return cantFichasTableroHorizontales;
     }
 
     public boolean isRotar() {
-        return ROTAR >= 5;
+        return cantFichasTableroHorizontales >= CANTFICHASHORIZONTALESMAX;
     }
 
-    public void setCantFichasTablero(int cantFichasTablero) {
-        this.cantFichasTablero = cantFichasTablero;
+    public void setCantFichasTablero(int cantFichasTableroHorizontales) {
+        this.cantFichasTableroHorizontales = cantFichasTableroHorizontales;
     }
 
     public void agregarFicha(VistaFicha ficha) {
-        if (cantFichasTablero < ROTAR) {
+        if (cantFichasTableroHorizontales < CANTFICHASHORIZONTALESMAX) {
             agregarFichasCentrales(ficha);
         } else {
-            agregarFichasCentrales(ficha, lastAddedX, lastAddedY);
+            agregarFichasCentrales(ficha, ultimoXAgregado, ultimoYAgregado);
         }
-        cantFichasTablero += 1;
+        cantFichasTableroHorizontales += 1;
     }
 
     private void agregarFichasCentrales(VistaFicha ficha) {
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = cantFichasTablero;
+        gbc.gridx = cantFichasTableroHorizontales;
         gbc.gridy = 0;
         gbc.insets = new Insets(0, 0, 0, 0);
         gbc.anchor = GridBagConstraints.CENTER;
-        if (ficha.getFicha().esFichaDoble()) {
-            System.out.printf("SARACATUNGAA IM INNNNN \n");
+        if (ficha.getFicha().esFichaDoble())
             gbc.insets = new Insets(0, -10, 0, -10);
-        }
         segundoPanel.add(ficha, gbc);
         segundoPanel.revalidate();
         segundoPanel.repaint();
-        lastAddedY++;
+        ultimoYAgregado++;
     }
 
     private void agregarFichasCentrales(VistaFicha ficha, int x, int y) {
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = cantFichasTablero -1;
+        gbc.gridx = CANTFICHASHORIZONTALESMAX - 1;
         gbc.gridy = y;
         gbc.insets = new Insets(0, 0, 0, 0);
         gbc.anchor = GridBagConstraints.CENTER;
-        if (ficha.getFicha().esFichaDoble()) {
-            System.out.printf("SARACATUNGAA IM INNNNN \n");
+        if (ficha.getFicha().esFichaDoble())
             gbc.insets = new Insets(0, -5, 0, -5);
-        }
         segundoPanel.add(ficha, gbc);
         segundoPanel.revalidate();
         segundoPanel.repaint();
 
         // Update the position of the last added tile
-        lastAddedX = cantFichasTablero -1;
-        lastAddedY = y + 1;
+        ultimoXAgregado = CANTFICHASHORIZONTALESMAX;
+        ultimoYAgregado = y + 1;
     }
     private void agregarFichasVerticales(VistaFicha ficha) {
 
