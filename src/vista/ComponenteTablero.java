ce0d304e484918cb911312a0510abccb1d46f518
frank <ochoa.franco132@gmail.com>
2024-07-17 18:56:30 -0300

    trying something out

--- src/vista/ComponenteTablero.java
+++ src/vista/ComponenteTablero.java
@@ -1,27 +1,14 @@
 package vista;
-
 import modelo.IFicha;
-
 import javax.swing.*;
 import java.awt.*;
 
-
 public class ComponenteTablero extends JPanel {
-    private int xCentral = 300;
-    private int yCentral = 50;
-    private int derechaVertX = 50;
-    private int derechaVertY = 50;
-    private int izquierdaVertX = 0;
-    private int izquierdaVertY = 100;
-    private int widthFicha = 50;
-    private int heightFicha = 100;
     private JPanel PCentral;
     private JPanel PVerticalIzq;
     private JPanel PVerticalDer;
     private JPanel PHorizontalArriba;
     private JPanel PHorizontalAbajo;
-
-
 
     public ComponenteTablero() {
         // caracteristicas del contenedor.
@@ -34,14 +21,21 @@ public class ComponenteTablero extends JPanel {
         PCentral.setBounds(100, 100, 650, 100);
         PCentral.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 15));
         PCentral.setOpaque(true);
-        PCentral.setBackground(Color.black);
+        PCentral.setBackground(Color.pink);
+        // Caracteristicas panel vertical derecho
+        PVerticalDer = new JPanel();
+        PVerticalDer.setSize(100, 100);
+        PVerticalDer.setBounds(650, 200, 100, 300);
+        PVerticalDer.setBackground(Color.BLACK);
+        PVerticalDer.setLayout(new BoxLayout(PVerticalDer, BoxLayout.Y_AXIS));
+        PCentral.setOpaque(true);
 
         PVerticalIzq = new JPanel();
-        PVerticalDer = new JPanel();
         PHorizontalArriba = new JPanel();
         PHorizontalAbajo = new JPanel();
 
         add(PCentral);
+        add(PVerticalDer);
 
     }
 
@@ -65,21 +59,18 @@ public class ComponenteTablero extends JPanel {
     }
 
     private void agregarFichasVertDerechas(VistaFicha ficha) {
-        derechaVertX = xCentral - widthFicha; // Align with the last central tile
-        System.out.println("Right Vertical Tile Position: (" + derechaVertX + ", " + derechaVertY + ")");
-        ficha.setBounds(derechaVertX, derechaVertY, widthFicha, heightFicha);
-        derechaVertY += heightFicha; // Move the next tile downthe next tile down
-
+        PVerticalDer.add(ficha);
+        revalidate();
+        repaint();
     }
 
     private void agregarFichasVerticalesIzquierdas(VistaFicha ficha) {
-        izquierdaVertX = xCentral; // Align with the last central tile
-        izquierdaVertY -= heightFicha;
-        ficha.setBounds(izquierdaVertX, izquierdaVertY, widthFicha, heightFicha);
+
     }
 
     public void limpiarFicha() {
         PCentral.removeAll();
+        PVerticalDer.removeAll();
         revalidate();
         repaint();
     }
